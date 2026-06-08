afe31688423d5584bbfb19f4e1258ad47f7792f5
Michael Gasperl <michael@gasperl.at>
2026-06-08 20:00:05 +0200

    Fix German label typos in tl_autolink
    - 'Fester' -> 'Fenster' (popup label + description)
    - 'Gross-' -> 'Groß-' (casesensitive description)
    - 'odere'/'mittles' -> 'oder'/'mittels' (cssID description)
    - 'Ihn' -> 'ihn' (description)
    - add missing commas in 'Klicken Sie hier, wenn ...'
    - replace stray map-extension leftover 'Eine neue Landkarte erstellen'
      with 'Einen neuen Link erstellen' (new-button description)

--- contao/languages/de/tl_autolink.php
+++ contao/languages/de/tl_autolink.php
@@ -12,20 +12,20 @@
  */
 
 $GLOBALS['TL_LANG']['tl_autolink']['tag']				= array('Suchbegriff', 'Geben Sie den Suchbegriff ein, welcher auf die unten ausgewählte Seite verlinkt werden soll.');
-$GLOBALS['TL_LANG']['tl_autolink']['description']		= array('Beschreibung', 'Sie können eine Beschreibung zu diesem Suchbegriff eingeben, um Ihn von gleichen Suchbegriffen unterscheiden zu können.');
+$GLOBALS['TL_LANG']['tl_autolink']['description']		= array('Beschreibung', 'Sie können eine Beschreibung zu diesem Suchbegriff eingeben, um ihn von gleichen Suchbegriffen unterscheiden zu können.');
 $GLOBALS['TL_LANG']['tl_autolink']['type']				= array('Typ', 'Bitte wählen Sie die gewünschte Link-Art aus.');
 $GLOBALS['TL_LANG']['tl_autolink']['page']				= array('Zielseite', 'Bitte wählen Sie die Zielseite.');
 $GLOBALS['TL_LANG']['tl_autolink']['url']				= array('URL', 'Bitte geben Sie die vollständige URL inklusive http:// ein.');
-$GLOBALS['TL_LANG']['tl_autolink']['popup']				= array('In neuem Fester öffnen', 'Klicken Sie hier wenn der Link in einem neuen Fester geöffnet werden soll.');
-$GLOBALS['TL_LANG']['tl_autolink']['casesensitive']		= array('Groß-/Kleinschreibung beachten', 'Klicken Sie hier an, wenn die Gross-/Kleinschreibung beim Suchbegriff beachtet werden soll.');
+$GLOBALS['TL_LANG']['tl_autolink']['popup']				= array('In neuem Fenster öffnen', 'Klicken Sie hier, wenn der Link in einem neuen Fenster geöffnet werden soll.');
+$GLOBALS['TL_LANG']['tl_autolink']['casesensitive']		= array('Groß-/Kleinschreibung beachten', 'Klicken Sie hier an, wenn die Groß-/Kleinschreibung beim Suchbegriff beachtet werden soll.');
 $GLOBALS['TL_LANG']['tl_autolink']['regex']				= array('Reguläre Ausdrücke verwenden', 'Klicken Sie hier an, wenn für den Suchbegriff Reguläre Ausdrücke zugelassen sind. Weitere Information finden Sie unter <a href="http://php.net/preg_replace" onclick="window.open(this.href); return false">http://php.net/preg_replace</a>.');
 $GLOBALS['TL_LANG']['tl_autolink']['selflink']			= array('Auf Zielseite anwenden', 'Klicken Sie hier an, wenn der Suchbegriff auch auf seiner eigenen Zielseite verlinkt werden soll.');
 $GLOBALS['TL_LANG']['tl_autolink']['cssFilter']			= array('Auswahl mittels CSS eingrenzen', 'Sie können die Suche auf gewisse Teile der Seite eingrenzen, indem Sie CSS-Argumente benutzen.');
 $GLOBALS['TL_LANG']['tl_autolink']['cssSelector']		= array('Selektor', 'Ein Selektor bestimmt, auf welchem HTML Element oder welcher Elementgruppe die Suche angewendet wird. Sie können einen oder mehrere durch Komma getrennte Selektoren erfassen.');
-$GLOBALS['TL_LANG']['tl_autolink']['cssID']				= array('Stylesheet-ID und -Klasse', 'Hier können Sie eine Stylesheet-ID (id attribute) sowie eine odere mehrere Stylesheet-Klassen (class attribute) eingeben, um den Link mittles CSS formatieren zu können.');
-$GLOBALS['TL_LANG']['tl_autolink']['addLanguage']		= array('Sprache zuordnen', 'Klicken Sie hier wenn dem Begriff eine Sprache zugeordnet werden soll. Dies kann z.B. für behindertengerechte Seiten hilfreich sein.');
+$GLOBALS['TL_LANG']['tl_autolink']['cssID']				= array('Stylesheet-ID und -Klasse', 'Hier können Sie eine Stylesheet-ID (id attribute) sowie eine oder mehrere Stylesheet-Klassen (class attribute) eingeben, um den Link mittels CSS formatieren zu können.');
+$GLOBALS['TL_LANG']['tl_autolink']['addLanguage']		= array('Sprache zuordnen', 'Klicken Sie hier, wenn dem Begriff eine Sprache zugeordnet werden soll. Dies kann z. B. für behindertengerechte Seiten hilfreich sein.');
 $GLOBALS['TL_LANG']['tl_autolink']['language']			= array('Sprache', 'Bitte wählen Sie eine Sprache.');
-$GLOBALS['TL_LANG']['tl_autolink']['addTip']			= array('Tool-Tip anzeigen', 'Klicken Sie hier wenn dem Begriff ein Tooltip hinterlegt werden soll.');
+$GLOBALS['TL_LANG']['tl_autolink']['addTip']			= array('Tool-Tip anzeigen', 'Klicken Sie hier, wenn dem Begriff ein Tooltip hinterlegt werden soll.');
 $GLOBALS['TL_LANG']['tl_autolink']['title']				= array('Titel', 'Geben Sie einen Titel für den Tooltip ein.');
 $GLOBALS['TL_LANG']['tl_autolink']['text']				= array('Text', 'Geben Sie einen Text für den Tooltip ein.');
 $GLOBALS['TL_LANG']['tl_autolink']['limitPages']		= array('Seiten limitieren', 'Klicken Sie hier, wenn Sie die automatische Verlinkung dieses Suchbegriffs auf gewisse Seiten limitieren wollen.');
@@ -47,7 +47,7 @@ $GLOBALS['TL_LANG']['tl_autolink']['none']				= 'Kein Link erstellen (nur CSS-Kl
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_autolink']['new']				= array('Neuer Link', 'Eine neue Landkarte erstellen');
+$GLOBALS['TL_LANG']['tl_autolink']['new']				= array('Neuer Link', 'Einen neuen Link erstellen');
 $GLOBALS['TL_LANG']['tl_autolink']['edit']				= array('Link bearbeiten', 'Link ID %s bearbeiten');
 $GLOBALS['TL_LANG']['tl_autolink']['copy']				= array('Link duplizieren', 'Link ID %s duplizieren');
 $GLOBALS['TL_LANG']['tl_autolink']['cut']				= array('Link verschieben', 'Link ID %s verschieben');
